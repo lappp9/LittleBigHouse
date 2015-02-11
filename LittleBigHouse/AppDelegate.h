bd3c4b3ec7efe0a4a9fce778868ca7b122c1086c
Luke Parham <lappp9@gmail.com>
2015-02-11 07:05:42 -0600

    Initial Commit

---
+++ LittleBigHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LittleBigHouse
+//
+//  Created by Luke Parham on 2/11/15.
+//  Copyright (c) 2015 Luke Parham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LittleBigHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LittleBigHouse
+//
+//  Created by Luke Parham on 2/11/15.
+//  Copyright (c) 2015 Luke Parham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
