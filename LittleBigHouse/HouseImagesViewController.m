8fc3473dd7d063c584c70db3d66127b497844da3
Luke Parham <lappp9@gmail.com>
2015-02-11 23:08:58 -0600

    fixed another button, need to use autolayout on the last page

---
+++ LittleBigHouse/HouseImagesViewController.m
@@ -0,0 +1,23 @@
+
+#import "HouseImagesViewController.h"
+
+@interface HouseImagesViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
+
+@end
+
+@implementation HouseImagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    _nextButton.layer.cornerRadius = 4;
+    [_nextButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    _nextButton.layer.shadowColor = [UIColor lightGrayColor].CGColor;
+    _nextButton.layer.shadowOpacity = 1.0;
+    _nextButton.layer.shadowOffset = CGSizeMake(1, 1);
+    UIColor *color = [UIColor colorWithRed:(52/255.f) green:(152/255.f) blue:(219/255.f) alpha:1.0];
+    _nextButton.backgroundColor = color;
+}
+
+@end
